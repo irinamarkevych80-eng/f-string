7873efca10482167edced9e5aabb29ec26a49823
irinamarkevych80-eng <irinamarkevych80@gmail.com>
2025-09-13 17:26:41 +0200

    Add f-strings.py

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # f-string
 
 Згадай про f-string 2025
+
+{var:[width][.precision][type]}
+
+type = d/f
---
+++ f-strings.py
@@ -0,0 +1,22 @@
+age = 20
+python_version = 3.13
+
+print(f"Your age is {age}")
+print(f"python_version={python_version}")
+
+number = 200
+print(f"{number:^8}$")
+
+x = 20.12384577437839939393
+print(f"{x:.5f}")
+
+year = 2025
+month = 9
+day = 5
+print(f"{day:0>2}.{month:0>2}.{year}")
+
+p = 28.123
+print(f"{p:.1f}%")
+
+m = 100_000_000
+print(f"{m:,}")
